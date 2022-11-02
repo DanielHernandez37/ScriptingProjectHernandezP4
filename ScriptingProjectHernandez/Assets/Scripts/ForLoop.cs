5730c9836e7775f5f80e82066071974072924055
DanielHernandez37 <710113@student.dusd.net>
2022-11-02 11:13:00 -0700

    loops
    loops warm up

--- ScriptingProjectHernandez/Assets/Scripts/ForLoop.cs
+++ ScriptingProjectHernandez/Assets/Scripts/ForLoop.cs
@@ -2,21 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Loops : MonoBehaviour
+public class ForLoop : MonoBehaviour
 {
-    int  cupsInTheSink = 4;
-
+    int numEnemies = 3;
     // Start is called before the first frame update
+
     void Start()
     {
-        while (cupsInTheSink > 0)
+        for (int i = 0; i < numEnemies; i++)
         {
-            Debug.Log("I've washed a cup!");
-            cupsInTheSink--;
-
+            Debug.Log("Creating enemy number: " + i);
         }
-
     }
 }
-
-
