c8bc08d11cc366ebc178112891ce94be1226c327
DanielHernandez37 <710113@student.dusd.net>
2022-11-15 10:55:24 -0800

    Scripting warm up
    Awake script warm up

---
+++ ScriptingProjectHernandez/Assets/Scripts/Awaking.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awaking : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start called.");
+    }
+
+    // Update is called once per frame
+    
+}
